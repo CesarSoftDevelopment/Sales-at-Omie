15b971f0eeecc9110ca153cd0c91782846fcceed
CesarSoftDevelopment <cesarsoares1997@gmail.com>
2024-09-16 18:00:53 -0300

    Creating UseCaseModule

--- app/src/main/java/com/cesarsoftdevelopment/omiesales/di/modules/UseCaseModule.kt
+++ app/src/main/java/com/cesarsoftdevelopment/omiesales/di/modules/UseCaseModule.kt
@@ -1,7 +1,49 @@
 package com.cesarsoftdevelopment.omiesales.di.modules
 
+import com.cesarsoftdevelopment.omiesales.domain.repository.ProductRepository
+import com.cesarsoftdevelopment.omiesales.domain.repository.SaleRepository
+import com.cesarsoftdevelopment.omiesales.domain.usecase.DeleteProductUseCase
+import com.cesarsoftdevelopment.omiesales.domain.usecase.GetProductsUseCase
+import com.cesarsoftdevelopment.omiesales.domain.usecase.GetSalesUseCase
+import com.cesarsoftdevelopment.omiesales.domain.usecase.SaveProductUseCase
+import com.cesarsoftdevelopment.omiesales.domain.usecase.SaveSaleUseCase
+import com.cesarsoftdevelopment.omiesales.domain.usecase.UpdateProductQuantityUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 class UseCaseModule {
 
+    @Provides
+    fun provideSaveProductUseCase(productRepository: ProductRepository): SaveProductUseCase {
+        return SaveProductUseCase(productRepository)
+    }
 
+    @Provides
+    fun provideGetProductsUseCase(productRepository: ProductRepository): GetProductsUseCase {
+        return GetProductsUseCase(productRepository)
+    }
 
+    @Provides
+    fun provideDeleteProductUseCase(productRepository: ProductRepository): DeleteProductUseCase {
+        return DeleteProductUseCase(productRepository)
+    }
+
+    @Provides
+    fun provideUpdateProductQuantityUseCase(productRepository: ProductRepository): UpdateProductQuantityUseCase {
+        return UpdateProductQuantityUseCase(productRepository)
+    }
+
+    @Provides
+    fun provideGetSalesUseCase(salesRepository: SaleRepository): GetSalesUseCase {
+        return GetSalesUseCase(salesRepository)
+    }
+
+    @Provides
+    fun provideSaveSaleUseCase(salesRepository: SaleRepository): SaveSaleUseCase {
+        return SaveSaleUseCase(salesRepository)
+    }
 }
